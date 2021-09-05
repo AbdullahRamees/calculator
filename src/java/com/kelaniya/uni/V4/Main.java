7569a26c4f42492013a4588ff1146b790c6c0214
Abdullah Ramees <m.abdullah.ramees0916@gmail.com>
2021-09-05 19:50:41 +0530

    week 4 V4

---
+++ src/java/com/kelaniya/uni/V4/Main.java
@@ -0,0 +1,35 @@
+package com.kelaniya.uni.v4;
+
+import com.kelaniya.uni.v4.operation.AddOperation;
+import com.kelaniya.uni.v4.operation.MulOperation;
+import com.kelaniya.uni.v4.operation.SubOperation;
+
+import java.io.IOException;
+
+public class Main {
+
+    //Refactoring the code
+    //Main class is the coordinator now...
+    public static void main(String[] args) throws IOException { //kinda wrong!
+
+        CommandLineInputs inputs = new CommandLineInputs(args);
+        String operator = inputs.getOperator();
+
+        FileReader fileReader = new FileReader();
+        Double[] numbers = fileReader.getNumbers();
+        Double number1 = numbers[0];
+        Double number2 = numbers[1];
+
+
+
+        //or use a if else statement
+        //coordination
+        OperationFactory operationFactory = new Operationfactory();
+        operation operation = operationFactory.getInstance(operator);
+        double result = operationperation.execute(numbers);
+
+       UI ui = new UI();
+       ui.showMessage("The result is "+ result)
+
+    }
+}
---
+++ src/java/com/kelaniya/uni/V4/UI.java
@@ -0,0 +1,7 @@
+package com.kelaniya.uni.v4;
+
+public class UI {
+    public static showMassage(string message){
+        system.out.println(message);
+    }
+}
